e943c0a318d23a2d2587d0fde3ff130c1c363efa
Richard Liu <richardadesigner@gmail.com>
2026-02-28 23:32:51 -0800

    update

--- binary-to-text/script.js
+++ binary-to-text/script.js
@@ -1,19 +1,38 @@
-document.getElementById("convert").addEventListener("click", function () {
-    const input = document.getElementById("input").value;
-    const inputArray = input.split(" ");
-    const TextCodeArray = inputArray.map(function (binary) {
-        return parseInt(binary, 2);
+window.addEventListener("load", function () {
+    let localStorageData = undefined;
+    if (localStorage.getItem("Project:textbinaryconverter")) {
+        localStorageData = JSON.parse(localStorage.getItem("Project:textbinaryconverter"));
+        if (localStorageData.inputOriginalBinarys) {
+            document.getElementById("input").value = localStorageData.inputOriginalBinarys || "";
+        }
+    } else {
+        localStorageData = {};
+        localStorageData.inputOriginalText = "";
+        localStorageData.inputOriginalBinarys = "";
+        localStorage.setItem("Project:textbinaryconverter", JSON.stringify(localStorageData));
+    }
+    document.getElementById("convert").addEventListener("click", function () {
+        const input = document.getElementById("input").value;
+        const inputArray = input.split(" ");
+        const TextCodeArray = inputArray.map(function (binary) {
+            return parseInt(binary, 2);
+        });
+        const TextCodeUint8Array = new Uint8Array(TextCodeArray);
+        const decoder = new TextDecoder();
+        const result = decoder.decode(TextCodeUint8Array);
+        document.getElementById("result").value = result;
     });
-    const TextCodeUint8Array = new Uint8Array(TextCodeArray);
-    const decoder = new TextDecoder();
-    const result = decoder.decode(TextCodeUint8Array);
-    document.getElementById("result").value = result;
+    document.getElementById("inputValueCopyBtn").addEventListener("click", function () {
+        document.getElementById("input").select();
+        navigator.clipboard.writeText(document.getElementById("input").value);
+    });
+    document.getElementById("resultValueCopyBtn").addEventListener("click", function () {
+        document.getElementById("result").select();
+        navigator.clipboard.writeText(document.getElementById("result").value);
+    });
+    document.getElementById("input").addEventListener("input", function () {
+        localStorageData.inputOriginalBinarys = this.value;
+        localStorage.setItem("Project:textbinaryconverter", JSON.stringify(localStorageData));
+    });
 });
-document.getElementById("inputValueCopyBtn").addEventListener("click", function () {
-    document.getElementById("input").select();
-    navigator.clipboard.writeText(document.getElementById("input").value);
-});
-document.getElementById("resultValueCopyBtn").addEventListener("click", function () {
-    document.getElementById("result").select();
-    navigator.clipboard.writeText(document.getElementById("result").value);
-});
+
--- text-to-binary/script.js
+++ text-to-binary/script.js
@@ -1,18 +1,36 @@
-document.getElementById("convert").addEventListener("click", function () {
-    const inputText = document.getElementById("input").value;
-    const encoder = new TextEncoder();
-    const encodedInputUint8Array = encoder.encode(inputText);
-    const encoedeInputArray = Array.from(encodedInputUint8Array);
-    const result = encoedeInputArray.map(function (i) {
-        return i.toString(2);
-    }).join(" ");
-    document.getElementById("result").innerText = result;
-});
-document.getElementById("inputValueCopyBtn").addEventListener("click", function () {
-    document.getElementById("input").select();
-    navigator.clipboard.writeText(document.getElementById("input").value);
-});
-document.getElementById("resultValueCopyBtn").addEventListener("click", function () {
-    document.getElementById("result").select();
-    navigator.clipboard.writeText(document.getElementById("result").value);
+window.addEventListener("load", function () {
+    let localStorageData = undefined;
+    if (localStorage.getItem("Project:textbinaryconverter")) {
+        localStorageData = JSON.parse(localStorage.getItem("Project:textbinaryconverter"));
+        if (localStorageData.inputOriginalText) {
+            document.getElementById("input").value = localStorageData.inputOriginalText || "";
+        }
+    } else {
+        localStorageData = {};
+        localStorageData.inputOriginalText = "";
+        localStorageData.inputOriginalBinarys = "";
+        localStorage.setItem("Project:textbinaryconverter", JSON.stringify(localStorageData));
+    }
+    document.getElementById("convert").addEventListener("click", function () {
+        const inputText = document.getElementById("input").value;
+        const encoder = new TextEncoder();
+        const encodedInputUint8Array = encoder.encode(inputText);
+        const encoedeInputArray = Array.from(encodedInputUint8Array);
+        const result = encoedeInputArray.map(function (i) {
+            return i.toString(2);
+        }).join(" ");
+        document.getElementById("result").innerText = result;
+    });
+    document.getElementById("inputValueCopyBtn").addEventListener("click", function () {
+        document.getElementById("input").select();
+        navigator.clipboard.writeText(document.getElementById("input").value);
+    });
+    document.getElementById("resultValueCopyBtn").addEventListener("click", function () {
+        document.getElementById("result").select();
+        navigator.clipboard.writeText(document.getElementById("result").value);
+    });
+    document.getElementById("input").addEventListener("input", function () {
+        localStorageData.inputOriginalText = this.value;
+        localStorage.setItem("Project:textbinaryconverter", JSON.stringify(localStorageData));
+    });
 });
